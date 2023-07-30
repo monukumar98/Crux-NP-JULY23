0078dbad75e30b29f136435b351a63f3538af72d
monukumar98 <monu012as@gmail.com>
2023-07-30 21:35:21 +0530

    Data type-2

---
+++ Lec7/Data_Type_2_Char_2.java
@@ -0,0 +1,19 @@
+package Lec7;
+
+public class Data_Type_2_Char_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch = 'a';
+		System.out.println((int) (ch));
+		int i = 1;
+		i++;// i = (int)(i+1);
+		i += 1;// i = (int)(i+1);
+		ch++;// ch = (char)(ch+1)
+		ch = (char) (ch + 1);
+		ch += 1;// ch = (char)(ch+1)
+		System.out.println(ch);
+
+	}
+
+}
---
+++ Lec7/For_Loops.java
@@ -0,0 +1,21 @@
+package Lec7;
+
+public class For_Loops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		for (byte b = 0; b < 128; b++) {
+//			System.out.println(b);
+//		}
+
+//		for (byte b = 0; b <= 127; b++) {
+//
+//			System.out.println(b);
+//		}
+		for (byte b = 0; b < 127; b++) {
+			System.out.println(b);
+		}
+
+	}
+
+}
