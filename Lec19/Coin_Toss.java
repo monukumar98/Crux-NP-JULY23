5dcec6dda49abfd0d5085d7604bc943439c3d233
monukumar98 <monu012as@gmail.com>
2023-09-18 11:28:03 +0530

    Recursion-2

---
+++ Lec19/Coin_Toss.java
@@ -0,0 +1,22 @@
+package Lec19;
+
+public class Coin_Toss {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 3;
+		Toss(n, "");
+
+	}
+
+	public static void Toss(int n, String ans) {
+		if (n == 0) {
+			System.out.print(ans + " ");
+			return;
+		}
+		Toss(n - 1, ans + "H");
+		Toss(n - 1, ans + "T");
+
+	}
+
+}
