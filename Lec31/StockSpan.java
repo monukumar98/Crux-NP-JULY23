513f279fc13538c3af303d6fd7112f61ca611021
monukumar98 <monu012as@gmail.com>
2023-11-04 14:49:35 +0530

    Stack QPs

---
+++ Lec31/StockSpan.java
@@ -0,0 +1,30 @@
+package Lec31;
+
+import java.util.Stack;
+
+public class StockSpan {
+	public static void main(String[] args) {
+		int[] arr = { 30, 35, 40, 38, 35 };
+		Cal_Span(arr);
+	}
+
+	public static void Cal_Span(int[] arr) {
+		Stack<Integer> st = new Stack<>();
+		int[] ans = new int[arr.length];
+		for (int i = 0; i < arr.length; i++) {
+			while (!st.isEmpty() && arr[i] > arr[st.peek()]) {
+				st.pop();
+			}
+			if (st.isEmpty()) {
+				ans[i] = i + 1;
+			} else {
+				ans[i] = i - st.peek();
+			}
+
+			st.push(i);
+		}
+		for (int i = 0; i < ans.length; i++) {
+			System.out.println(arr[i] + "," + ans[i]);
+		}
+	}
+}
